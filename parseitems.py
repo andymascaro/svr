d4f6dfc5c340c5ae7f98bb4845ee59ebf0fd575b
intersectdigital-am <amascaro@intersectdigital.com>
2023-04-05 21:45:11 -0500

    items

---
+++ parseitems.py
@@ -0,0 +1,64 @@
+import json
+
+#from gamedata import *
+
+
+def json_to_english(json_data):
+    descriptions = []
+    for obj in json_data:
+        #if 'img' in obj:
+        #    description += f"Image: {obj['img']}\n"
+        #if 'loc' in obj:
+        #    x, y, z = obj['loc']
+        #    description += f"Location: ({x}, {y}, {z})\n"
+        description = ""
+
+        name = obj.get('display') or obj.get('name')
+        description += f"An item named {name} exists. "
+
+        #if 'cash' in obj:
+        #    description += f"Cash: {obj['cash']}\n"
+        if 'description' in obj:
+            description += f"{name} has this property: \"{obj['description']}\" "
+        #if 'quests' in obj:
+        #    description += f"Quests: {', '.join(obj['quests'])}\n"
+        #if 'values' in obj:
+        #    description += f"Values: {', '.join(map(str, obj['values']))}\n"
+        #if 'wants' in obj:
+        #    description += f"Wants: {', '.join(map(str, obj['wants']))}\n"
+        #if 'resell' in obj:
+        #    description += f"Resell Value: {obj['resell']}\n"
+        
+        descriptions.append(description)
+    return "".join(descriptions)
+
+# Example usage
+json_data = [
+	{
+		"name": "oil",
+		"display": "Cooking-grade Oil",
+		"base": 1,
+		"drop": 1,
+	    "tags": ["spice"]
+	},
+	{
+		"name": "fat",
+		"display": "Rendered Animal Fat",
+		"base": 50,
+		"drop": 1,
+	    "tags": ["spice"]
+	}
+]
+
+
+with open('output.json', 'r') as f:
+    # Load the JSON data into a Python dictionary
+    json_data = json.load(f)
+    json_data = json.loads(json_data)
+
+    
+print(json_to_english(json_data))
+
+with open('items.txt', 'w') as f:
+    # Write some text to the file
+    f.write(json_to_english(json_data))
--- procjson.py
+++ procjson.py
@@ -11,32 +11,36 @@ def replace_inner_quotes(match):
 
 def fix_json_formatting(json_str):
     # Remove single-line comments (//comment)
-    #json_str = re.sub(r'//.*', '', json_str)
+    json_str = re.sub(r'//.*', '', json_str)
 
     # Remove multi-line comments (/* hey heres a comment */)
-    #json_str = re.sub(r'/\*.*?\*/', '', json_str, flags=re.DOTALL)
-    
-    # Remove whitespace before opening brackets ([ and {)
-    #json_str = re.sub(r'\s*([\[\{])\s*', r'\1', json_str)
-
-    # Remove whitespace after closing brackets (] and })
-    #json_str = re.sub(r'\s*([\]\}])\s*', r'\1', json_str)
-
-    # Remove whitespace around commas
-    #json_str = re.sub(r'\s*,\s*', ',', json_str)
-
-    # Remove trailing commas before closing brackets (] and })
-    #json_str = re.sub(r',\s*([\]\}])', r'\1', json_str)
-
+    json_str = re.sub(r'/\*.*?\*/', '', json_str, flags=re.DOTALL)
 
     # Fix numbers
     json_str = re.sub(r'(?:(\d)|(\s|-))\.(?:(\d)|(\s))', fix_json_numbers, json_str)
 
-    # Replace single quotes with double quotes, only when not inside double quotes
-    json_str = re.sub(r'(?<!")\'(?!")([^"]+)(?<!")\'(?!")', r'"\1"', json_str)
+    # Remove whitespace before opening brackets ([ and {)
+    json_str = re.sub(r'\s*([\[\{])\s*', r'\1', json_str)
+
+    # Remove whitespace after closing brackets (] and })
+    json_str = re.sub(r'\s*([\]\}])\s*', r'\1', json_str)
+
+    # Remove whitespace around commas
+    json_str = re.sub(r'\s*,\s*', ',', json_str)
+
+    # Remove whitespace around :
+    json_str = re.sub(r'\s*:\s*', ':', json_str)
+
+    # Remove trailing commas before closing brackets (] and })
+    json_str = re.sub(r',\s*([\]\}])', r'\1', json_str)
+
+
+
+    # Replace single quotes with double quotes
+    json_str = re.sub(r'\'', r'"', json_str)
 
     # Replace any remaining double quotes inside double-quoted strings with single quotes
-    json_str = re.sub(r'"[^"]*"', replace_inner_quotes, json_str)
+    json_str = re.sub(r'(?<=":").+?(?=",)', replace_inner_quotes, json_str)
 
     # Wrap any non-number value in double quotes
     #json_str = re.sub(r':\s*([^"])([^\d\s,]+)([^"])', r': "\2"', json_str)
@@ -53,13 +57,13 @@ def fix_json_file(input_file, output_file):
         fixed_json = json.loads(fixed_json_str)
     except json.JSONDecodeError as e:
         print(f"Error parsing JSON: {e}")
-        #return
+        return
 
-    with open(output_file, 'w') as f:
-        f.write(fixed_json_str)
-    
     #with open(output_file, 'w') as f:
-    #    json.dump(fixed_json_str, f, indent=2)
+    #    f.write(fixed_json_str)
+    
+    with open(output_file, 'w') as f:
+        json.dump(fixed_json_str, f, indent=2)
 
 if __name__ == "__main__":
     input_file = "input.json"
